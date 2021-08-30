24756d51298cd4269f74c9c1a7888c9cce08c8f3
goodmean <zizigoo12@gmail.com>
2021-08-30 11:49:05 +0900

    DBUtil 적용, 소스코드 다이어트

--- src/com/sbs/example/textBoard/Article.java
+++ src/com/sbs/example/textBoard/Article.java
@@ -1,13 +1,15 @@
-package com.sbs.example.textBorad;
+package com.sbs.example.textBoard;
 
-public class Article extends Object{
+import java.util.Map;
+
+public class Article extends Object {
 
 	public int id;
 	public String regDate;
 	public String updateDate;
 	public String title;
 	public String body;
-	
+
 	public Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
@@ -21,12 +23,17 @@ public class Article extends Object{
 		this.title = title;
 		this.body = body;
 	}
-	
+
+	public Article(Map<String, Object> articleMap) {
+		this.id = (int) articleMap.get("id");
+		this.title = (String) articleMap.get("title");
+		this.body = (String) articleMap.get("body");
+	}
+
 	@Override
 	public String toString() {
 		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
 				+ ", body=" + body + "]";
 	}
-	
-	
+
 }
--- src/com/sbs/example/textBoard/Main.java
+++ src/com/sbs/example/textBoard/Main.java
@@ -1,4 +1,4 @@
-package com.sbs.example.textBorad;
+package com.sbs.example.textBoard;
 
 public class Main {
 	public static void main(String[] args) {
