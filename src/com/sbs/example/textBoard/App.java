7209e3da6712b1d0b18b9c8b3555053ad24283c7
goodmean <zizigoo12@gmail.com>
2021-08-31 12:08:11 +0900

    게시물 상세보기 기능 구현

--- src/com/sbs/example/textBoard/App.java
+++ src/com/sbs/example/textBoard/App.java
@@ -108,6 +108,30 @@ public class App {
 			DBUtil.delete(conn, sql);
 
 			System.out.printf("%d번 게시글이 삭제되었습니다.\n", id);
+			
+		} else if (command.startsWith("article detail ")) {
+			int id = Integer.parseInt(command.split(" ")[2]);
+
+			System.out.printf("== %d번 게시글 상세보기 ==\n", id);
+
+			SecSql sql = new SecSql();
+			sql.append("SELECT *");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?", id);
+			Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+
+			if (articleMap.isEmpty()) {
+				System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+				return 0;
+			}
+
+			Article article = new Article(articleMap);
+
+			System.out.printf("번호 : %d\n", article.id);
+			System.out.printf("작성날짜 : %s\n", article.regDate);
+			System.out.printf("수정날짜 : %s\n", article.updateDate);
+			System.out.printf("제목 : %s\n", article.title);
+			System.out.printf("내용 : %s\n", article.body);
 
 		} else if (command.startsWith("article modify ")) {
 			int id = Integer.parseInt(command.split(" ")[2]);
@@ -124,7 +148,7 @@ public class App {
 
 			sql.append("UPDATE article");
 			sql.append("SET updateDate = NOW()");
-			sql.append(", titl = ?", title);
+			sql.append(", title = ?", title);
 			sql.append(", `body` = ?", body);
 			sql.append("WHERE id = ?", id);
 
